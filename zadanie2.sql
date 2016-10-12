1e179e3dbc191a839e09e6d8f7abf3512e199c99
SeninVladimir <darprod_ent@mail.ru>
2016-10-13 00:16:26 +0400

    Update zadanie2.sql

--- zadanie2.sql
+++ zadanie2.sql
@@ -1,3 +1,6 @@
 SELECT `CustomerID` , `ProductId` , MIN( `DateCreated` ) 
 FROM `sales` 
-GROUP BY `CustomerID` 
+GROUP BY `CustomerID`
+
+--Единственная СУБД, которая была под рукой - это MYSQL.
+--Поэтому для нее и написал запрос.
